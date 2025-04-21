c653e459b36f8fe82a197d2214c849b0e6be636f
sugarflowers <128127805+sugarflowers@users.noreply.github.com>
2025-04-21 17:30:31 +0900

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -91,7 +91,8 @@ impl SerialComm {
 
                 // 改行があったらラインバッファをクリアする。
                 if let Some(index) = buffer.iter().position(|&x| x == 0x0A || x == 0x0D ) {
-                    let linebuffer:Vec<u8> = linebuffer[index+1..].to_vec();
+                    //let linebuffer:Vec<u8> = linebuffer[index+1..].to_vec();
+                    let mut linebuffer:Vec<u8> = Vec::new();
                 }
             }
         }
